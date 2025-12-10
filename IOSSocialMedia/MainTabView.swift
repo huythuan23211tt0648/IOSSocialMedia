b9135c2316a7bc2372c9a249ed1c94fc943c784b
dongBE2k5 <yivictor2005@gmail.com>
2025-12-10 23:25:14 +0700

    update

---
+++ IOSSocialMedia/MainTabView.swift
@@ -0,0 +1,54 @@
+//
+//  MainTabView.swift
+//  IOSSocialMedia
+//
+//  Created by cao_dong on 10/12/25.
+//
+import SwiftUI
+
+struct MainTabView: View {
+    @StateObject var auth = AuthViewModel()
+    
+    var body: some View {
+        TabView {
+
+            NavigationView {
+                HomeView()
+//                    .navigationTitle("Home")
+            }
+            .tabItem {
+                Image(systemName: "house.fill")
+                Text("Home")
+            }
+
+            NavigationView {
+                CreateView()
+                    .navigationTitle("Create")
+            }
+            .tabItem {
+                Image(systemName: "plus.square")
+                Text("Create")
+            }
+
+            NavigationView {
+                MessagesListView()
+                    .navigationTitle("Messages")
+            }
+            .tabItem {
+                Image(systemName: "paperplane")
+                Text("Messages")
+            }
+
+            NavigationView {
+                ProfileView()
+                    .environmentObject(auth)
+                    .navigationTitle("Profile")
+            }
+            .tabItem {
+                Image(systemName: "person.crop.circle")
+                Text("Profile")
+            }
+        }
+    }
+}
+
