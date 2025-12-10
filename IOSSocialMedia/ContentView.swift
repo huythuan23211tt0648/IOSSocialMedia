c6c739e8ba20a9d0b0aab243989a229bb2e54b70
huythuan23211tt0648 <huythuan7a4gmail.com>
2025-12-10 05:18:18 -0800

    Initial Commit

---
+++ IOSSocialMedia/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  IOSSocialMedia
+//
+//  Created by thuan on 10/12/2025.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ IOSSocialMedia/IOSSocialMediaApp.swift
@@ -0,0 +1,17 @@
+//
+//  IOSSocialMediaApp.swift
+//  IOSSocialMedia
+//
+//  Created by thuan on 10/12/2025.
+//
+
+import SwiftUI
+
+@main
+struct IOSSocialMediaApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
