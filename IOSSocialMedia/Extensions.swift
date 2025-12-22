89d03acac16bec34d2bc8d0d3ce91594e4534be9
dongBE2k5 <yivictor2005@gmail.com>
2025-12-23 01:27:26 +0700

    done 80

---
+++ IOSSocialMedia/Extensions.swift
@@ -0,0 +1,18 @@
+//
+//  Extensions.swift
+//  IOSSocialMedia
+//
+//  Created by cao_dong on 22/12/25.
+//
+
+import SwiftUI
+
+struct Extensions: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    Extensions()
+}
---
+++ IOSSocialMedia/Views/Profile/OtherUserProfileView.swift
@@ -0,0 +1,18 @@
+//
+//  OtherUserProfileView.swift
+//  IOSSocialMedia
+//
+//  Created by cao_dong on 23/12/25.
+//
+
+import SwiftUI
+
+struct OtherUserProfileView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    OtherUserProfileView()
+}
