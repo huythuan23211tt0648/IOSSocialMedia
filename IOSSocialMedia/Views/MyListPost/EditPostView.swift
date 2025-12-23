14bcf7a051418088a07e0f76ff26e8faa8e58718
dongBE2k5 <yivictor2005@gmail.com>
2025-12-23 11:55:49 +0700

    done 95

---
+++ IOSSocialMedia/Views/MyListPost/EditPostView.swift
@@ -0,0 +1,18 @@
+//
+//  EditPostView.swift
+//  IOSSocialMedia
+//
+//  Created by cao_dong on 23/12/25.
+//
+
+import SwiftUI
+
+struct EditPostView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    EditPostView()
+}
